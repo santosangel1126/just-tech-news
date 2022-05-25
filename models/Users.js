83751f01d6addb546d29f0ba58e0fa5c6d787250
Angel Santos <iiisantosjangel@gmail.com>
2022-05-24 19:12:10 -0700

    updated packages

--- models/Users.js
+++ models/Users.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
+const bcrypt = require('bcrypt');
 // create our User model
 class User extends Model {}
 
